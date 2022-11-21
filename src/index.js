80e947dc605a46f0349309e18a969dcca9924b0b
Nikita <nikitakoulaga@gmail.com>
2022-11-21 16:37:52 -0500

    added vercel audience tracking

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+//vercel audience analytics
+import { Analytics } from '@vercel/analytics/react';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
